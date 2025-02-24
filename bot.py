3c636853da797a8bb555db4d7fed58ec05f87b69
Μιθριδάτης <game.developer.mb@gmail.com>
2025-02-24 21:28:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import os
 import logging
 import sqlite3
-from telegram import Update, ParseMode
+from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import (
     Application, CommandHandler, MessageHandler, filters, CallbackContext
 )
